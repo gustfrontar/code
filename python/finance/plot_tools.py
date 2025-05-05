871594985abb6d4e303b3bdb4553afd2840bbf0c
gustfrontar <juan.j.ruiz.ar@gmail.com>
2025-05-05 19:06:36 +0000

    Update finance

--- python/finance/plot_tools.py
+++ python/finance/plot_tools.py
@@ -2,18 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_pair( pair , plot_path , show = True , tailind = 1000) :
+def possition_summary( pair ) :
+
+    print('This is a dummy function') 
+
+
+
+def plot_pair( pair , plot_path , show = True , tailind = 5000) :
 
     price1 = pair['Price1']
     price2 = pair['Price2']
     name1  = pair['Comp1']
     name2  = pair['Comp2']
     zscore = pair['z_scores']
-    zscore_trade = pair['z_scores_trading']
-    zscore_ini   = pair['z_scores_ini']
-    zscore_end   = pair['z_scores_end']
+    zscore_trade = pair['ZScoreTrading']
+    zscore_open  = np.array( pair['ZScoreOpen']  )
+    zscore_close = np.array( pair['ZScoreClose'] )
     possition = pair['Possition']
-    possition_return = pair['Possition_return']
+    possition_return = np.array( pair['PossitionReturn'] )
     plt.figure()
     plt.plot( ( price1[-tailind:] - np.nanmean(price1[-tailind:]) ) / np.nanstd( price1[-tailind:]) , 'b' ,label=name1)
     plt.plot( ( price2[-tailind:] - np.nanmean(price2[-tailind:]) ) / np.nanstd( price2[-tailind:]) , 'r' ,label=name2) 
@@ -31,11 +37,11 @@ def plot_pair( pair , plot_path , show = True , tailind = 1000) :
 
 
     plt.figure()
-    tmp = np.copy( possition_return[-tailind:] )
+    #tmp = np.copy( possition_return[-tailind:] )
 
-    tmp[tmp==0 ] = np.nan
+    #tmp[tmp==0 ] = np.nan
     plt.plot( possition[-tailind:] , 'r' , label='Possition')
-    plt.plot( tmp , 'ok' , label='Possition return')
+    #plt.plot( tmp , 'ok' , label='Possition return')
     plt.plot
     plt.legend()
     plt.title('Possition (+/-)')
@@ -48,13 +54,8 @@ def plot_pair( pair , plot_path , show = True , tailind = 1000) :
     plt.title('Cumulative return (%)')
     plt.savefig( plot_path + '/PossitionRet_'  + name1 + '.' + name2 + '.png')
 
-
-    tmp = np.copy( possition_return ) 
-    tmp2 = np.copy( zscore_ini )
-    tmp3 = np.copy( possition  )
-
     plt.figure()
-    plt.scatter( possition_return , zscore_ini , c=zscore_end )
+    plt.scatter( possition_return , zscore_open , c=zscore_close )
     plt.xlabel('Return %')
     plt.ylabel('Initial z-score')
     plt.savefig( plot_path + '/ScatterRet_'  + name1 + '.' + name2 + '.png')
--- python/finance/tools.py
+++ python/finance/tools.py
@@ -108,7 +108,7 @@ def process_pairs( price , pair_index , company , back_time = 25 ) :
     return pairs   
 
 
-def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
+def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 , length_thresh = 100 , max_z_score = 10 ) :
     #Apply a simple trading strategy.
 
     pairs = pairs_data.keys() 
@@ -120,7 +120,7 @@ def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
     for ip in range( npairs )  :
 
        print('Processing pair ', ip )
-       print('Companies ',pairs_data[str(ip)]['Comp1'],' ',pairs_data[str(ip)]['Comp1'])
+       print('Companies ',pairs_data[str(ip)]['Comp1'],' ',pairs_data[str(ip)]['Comp2'])
 
        #Time loop.
        price1 = pairs_data[str(ip)]['Price1']
@@ -129,8 +129,8 @@ def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
        betha  = pairs_data[str(ip)]['betha']
        alpha  = pairs_data[str(ip)]['alpha']
        zscore = np.zeros( len(betha) ) + np.nan
-       zscore_ini = np.copy( zscore ) 
-       zscore_end = np.copy( zscore ) 
+       #zscore_ini = np.copy( zscore ) 
+       #zscore_end = np.copy( zscore ) 
        #spread = np.zeros( len(betha) ) + np.nan
        back_time = pairs_data[str(ip)]['BackTime']
 
@@ -138,22 +138,42 @@ def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
 
        possition = np.zeros( ntimes )  #1 if open possitive, -1 if open negative, 0 if close.
 
-       possition_return = np.zeros( ntimes ) #Percentual return at possition closure.
+       #possition_return = np.zeros( ntimes ) #Percentual return at possition closure.
 
+       possition_type = list()
+       possition_return = list()
+       index_open = list()
+       index_close = list()
+       price_open = list()
+       price_close = list()
+       percreturn = list()
+       possalpha = list()
+       possbetha = list()
+       possprice1 = list()
+       possprice2 = list()
+       posszthresh_open = list()
+       posszthresh_close= list()
+       zscore_open = list()
+       zscore_close = list()
 
        for ii in range(1,ntimes) :
           if possition[ii] == 0  :
             #When the possition is closed we update alpha and betha  
             cbetha = betha[ii]
             calpha = alpha[ii]
-
+            min_time = ii - back_time
+            if min_time < 0 :
+               min_time = 0
+            cspread = price2[min_time:ii] - cbetha * price1[min_time:ii] - calpha
+            meanspread = np.nanmean( cspread )
+            stdspread  = np.nanstd( cspread )
           #Recompute zscore .... (fix alpha and betha when we open a possition)
           #update alpha and betha when the possition is closed
           min_time = ii - back_time
           if min_time < 0 :
              min_time = 0
-          spread = price2[min_time:ii] - cbetha * price1[min_time:ii] - calpha
-          zscore[ii] = ( spread[-1] - np.nanmean( spread ) ) / np.nanstd( spread )
+          spread = price2[ii] - cbetha * price1[ii] - calpha
+          zscore[ii] = ( spread - meanspread ) / stdspread
 
           if ( np.abs( zscore[ii] ) > zthresh_open ) and possition[ii] == 0 :
             #Open possition
@@ -161,19 +181,29 @@ def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
                possition[ii:] = 1 
             if zscore[ii] < 0 :
                possition[ii:] = -1
+            cindex_open = ii
             #Save the prices at the possition opening
             price_open_1 = price1[ii]
             price_open_2 = price2[ii]
             czscore_ini = zscore[ii]
 
-
-          elif( possition[ii] == 1 and zscore[ii] < zthresh_close ) or ( possition[ii] == -1 and zscore[ii] > -1.0*zthresh_close ):
+          #Posstion is closed if the zscore goes close to zero. 
+          #Or if the length of the possition in time exceeds the length_thresh 
+          #Or if the zscore is over the maximum allowed.
+          elif( possition[ii] == 1 and zscore[ii] < zthresh_close ) or ( possition[ii] == -1 and zscore[ii] > -1.0*zthresh_close ) or ( np.abs( possition[ii]) == 1 and np.abs(zscore[ii]) > max_z_score ) or ( np.abs(possition[ii]) == 1 and ii - cindex_open > length_thresh ) :
             #Get the return
             return1 = possition[ii] * ( price1[ii] - price_open_1 ) / price_open_1 
             return2 = possition[ii] * ( price_open_2 - price2[ii] ) / price_open_2
-            possition_return[ii] = return1 + return2 
-            zscore_ini[ii] = czscore_ini #Store the associated initial zscore for this possition
-            zscore_end[ii] = zscore[ii]
+            possition_return.append( ( return1 + return2 ) / 2.0 )
+            possition_type.append( possition[ii] )
+            zscore_open.append( czscore_ini ) #Store the associated initial zscore for this possition
+            zscore_close.append( zscore[ii]  )
+            index_open.append( cindex_open )
+            index_close.append( ii )
+            price_open.append(  np.array( price_open_1 , price_open_2 ) )
+            price_close.append( np.array( price1[ii] , price2[ii] ) )
+            posszthresh_open.append( zthresh_open )
+            posszthresh_close.append( zthresh_close )
             #print( return1 + return2 )
             #Close possition
             if ii + 1 < ntimes - 1 :
@@ -181,10 +211,17 @@ def trading_strategy( pairs_data , zthresh_open = 0.7 , zthresh_close = 0.1 ) :
 
           
        pairs_data[str(ip)]['Possition'] = possition
-       pairs_data[str(ip)]['Possition_return'] = possition_return
-       pairs_data[str(ip)]['z_scores_trading'] = zscore #The actual z_score used in the trading strategy
-       pairs_data[str(ip)]['z_scores_ini'] = zscore_ini #The zscore at the opening of each possition 
-       pairs_data[str(ip)]['z_scores_end'] = zscore_end #The zscore at the closing of each possition
+       pairs_data[str(ip)]['PossitionReturn'] = possition_return
+       pairs_data[str(ip)]['PossitionType']  = possition_return
+       pairs_data[str(ip)]['ZScoreTrading'] = zscore #The actual z_score used in the trading strategy
+       pairs_data[str(ip)]['ZScoreOpen']     = zscore_open #The zscore at the opening of each possition 
+       pairs_data[str(ip)]['ZScoreClose']    = zscore_close #The zscore at the closing of each possition
+       pairs_data[str(ip)]['IndexOpen']    = index_open
+       pairs_data[str(ip)]['IndexClose']    = index_close
+       pairs_data[str(ip)]['PriceOpen']    = price_open
+       pairs_data[str(ip)]['PriceClose']    = price_close
+       pairs_data[str(ip)]['PossZThreshOpen']    = posszthresh_open
+       pairs_data[str(ip)]['PossZThreshClose']    = posszthresh_close
 
 
     return pairs_data 
